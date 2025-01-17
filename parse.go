ccb8522697751fc204fb2971b4d307f5c208ee5f
Shenal Fernando <shenal.fernando10@gmail.com>
2025-01-17 20:03:04 +0530

    [#17] comment updated

--- parse.go
+++ parse.go
@@ -18,7 +18,8 @@ import "github.com/caarlos0/env/v11"
 //
 //		type Config struct {
 //		    Name string `env:"MY_NAME"`
-//		    Age  int    `env:"MY_AGE"` `
+//		    Age  int    `env:"MY_AGE"`
+//	     	Team string `env:"MY_TEAM" envDefault:"backend"`
 //		}
 //
 //		var conf Config
